37a2d53d095a47fdb4232b64e1b594fa436d0d00
Mason Guaman <masonstalin@gmail.com>
2023-01-18 14:14:46 -0500

    ty

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -7,13 +7,60 @@
 
 import UIKit
 
+
+enum Mode {
+    case flashCard
+    case quiz
+}
+
 class ViewController: UIViewController {
+    
+    
+    var mode: Mode = .flashCard
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateElement()
+        
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    
 
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    
+    var currentElementIndex = 0
+    
+    func updateElement() {
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        imageView.image = image; answerLabel.text = "?"
+    }
+    
+    
+    @IBAction func showAnswer(_ sender: Any) {
+        answerLabel.text = elementList[currentElementIndex]
+    }
+    
+    
+    @IBAction func next(_ sender: Any) {
+        currentElementIndex += 1
+        if currentElementIndex > 3{
+            currentElementIndex = 0
+            updateElement()
+        }
+        
+        updateElement()
+    }
+    
+    
 }
 
